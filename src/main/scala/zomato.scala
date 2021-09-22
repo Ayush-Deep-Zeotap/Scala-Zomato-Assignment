c5d1d59cc220c1604622cd3862fe794321ea029e
Ayush-Deep-Zeotap <ayush.deep@zeotap.com>
2021-09-22 15:06:34 +0530

    Address PR Review Changes

--- src/main/scala/zomato.scala
+++ src/main/scala/zomato.scala
@@ -1,92 +1,94 @@
-import scala.collection.mutable._
+import scala.io.Source
 
 object zomato extends App {
 
-  case class Restaurant(id: String = "Nil", restaurantName: String = "", rating: String = "0.0/5.0", numberOfVotes: String = "0", location: String = "Unknown", restaurantType: String = "Nil",
-                        dishesLiked: String = "", typesOfCuisines: String = "", costForTwo: String = "Nil")
-
-  val zomatoData = ListBuffer[Restaurant]()
+  case class Restaurant(id: String, restaurantName: String, rating: Double = 0.0, numberOfVotes: Long = 0L, location: String, restaurantType: String,
+                        dishesLiked: List[String], typesOfCuisine: List[String], costForTwo: String)
 
   // Function to Read the Data
-  def readData() = {
+  def readData(): List[Restaurant] = {
 
-    def splitRow(row: String): List[String] = {
-      row.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1).toList
-    }
+    val src = Source.fromFile("data/zomato.csv")
+    val lines = src.getLines().map(x => x.split(',')).toList
+    src.close()
+    val validResturants = lines.filter(x => x.length==9)
+    validResturants.map(x => Restaurant(x(0),x(1),
+      try{
+        x(2).split('/').head.toDouble}
+      catch{
+        case e : NumberFormatException => 0.0
+    },x(3) match {
+        case votes if(votes.isEmpty) => 0L
+        case _ => x(3).toLong
+      },x(4),x(5),x(6) match {
+        case dishesLiked if(dishesLiked.isEmpty) => List()
+        case _ => x(6).split(',').toList
+      },x(7) match {
+        case cuisineList if(cuisineList.isEmpty) => List()
+        case _ => x(7).split(' ').toList
+      },x(8) match {
+        case costForTwo if(costForTwo.isEmpty) => "Nil"
+        case _ =>  x(8)
+      }))
 
-    val bufferedSource = io.Source.fromFile("data/zomato.csv")
-    for (line <- bufferedSource.getLines) {
-
-      val row = splitRow(line)
-
-      var validRating = ""
-
-      if (row(2) == "NEW") validRating = "0.0/5.0"
-
-      else validRating = row(2)
-
-      val parsedRow = Restaurant(row.head, row(1), validRating, row(3), row(4), row(5), row(6), row(7), row(8))
-
-      zomatoData += parsedRow
-    }
-    bufferedSource.close()
   }
 
   // Top N restaurants by rating
-  def sortByRating(N: Int, restaurantList: ListBuffer[Restaurant]): ListBuffer[Restaurant] = restaurantList.sortBy(x => x.rating.split('/').head.toDouble).reverse.take(N)
+  def sortByRating(N: Int, restaurantList: List[Restaurant]): List[Restaurant] = restaurantList.sortBy(x => x.rating).reverse.take(N)
 
   // Top N restaurants by rating in a given location and restaurant type
-  def locationAndTypeFilter(N: Int, restaurantList: ListBuffer[Restaurant], reqType: String, reqLocation: String): ListBuffer[Restaurant] =
-    restaurantList.filter(x => x.location.contains(reqLocation)).filter(x => x.restaurantType.contains(reqType)).take(N)
+  def locationAndTypeFilter(N: Int, restaurantList: List[Restaurant], reqType: String, reqLocation: String): List[Restaurant] =
+    restaurantList.filter(x => x.location.equals(reqLocation) && x.restaurantType.equals(reqType)).sortBy(x => x.rating).reverse.take(N)
 
   // Top N restaurants by rating and least number of votes in a given location
-  def filterByVote(N: Int, restaurantList: ListBuffer[Restaurant], reqLocation: String): ListBuffer[Restaurant] = {
+  def filterByVote(N: Int, restaurantList: List[Restaurant], reqLocation: String): List[Restaurant] = {
 
     val restaurantsAtLocation = restaurantList.filter(x => x.location.contains(reqLocation))
 
-    restaurantsAtLocation.sortBy(x => x.numberOfVotes.toInt).sortWith(_.rating.split('/').head.toDouble > _.rating.split('/').head.toDouble)
+    restaurantsAtLocation.sortBy(x => x.numberOfVotes.toInt).sortWith(_.rating > _.rating)
 
   }
 
   // No. of dishes liked in every restaurant
-  def likedDishCount(restaurantList : ListBuffer[Restaurant]): ListBuffer[Int] = {
+  def likedDishCount(restaurantList : List[Restaurant]): List[Int] = {
 
-    restaurantList.map(x => x.dishesLiked.split( " ").length)
+    restaurantList.map(x => x.dishesLiked.distinct.length)
 
   }
 
   //No. of distinct locations
-  def distinctLocationCount(restaurantList : ListBuffer[Restaurant]): Int = {
+  def distinctLocationCount(restaurantList : List[Restaurant]): Int = {
 
-    val locations = restaurantList.map(x => x.location)
-
-    locations.distinct.length
+    restaurantList.map(x => x.location).distinct.length
 
   }
 
   // No. of distinct cuisines at a certain location
-  def distinctCuisine(restaurantList : ListBuffer[Restaurant],reqLocation: String): Int = {
+  def distinctCuisine(restaurantList : List[Restaurant],reqLocation: String): Int = {
 
-    restaurantList.filter(x => x.location.contains(reqLocation)).groupBy(_.typesOfCuisines.split(" ")).size
+    restaurantList.filter(x => x.location==reqLocation).map(x => x.typesOfCuisine).distinct.length
 
   }
 
   //No. of distinct cuisines at each location
-  def allDistinctCuisine(restaurantList : ListBuffer[Restaurant]): Any = {
+  def allDistinctCuisine(restaurantList : List[Restaurant]): Map[String,Int] = {
 
-    restaurantList.groupBy(_.location).map(x => x._2.length)
+    restaurantList.groupBy(_.location).map({case (k,v) => k -> v.map(x => x.typesOfCuisine).distinct.length})
 
   }
 
   //Count of restaurants for each cuisine type
-  def countByCuisine(restaurantList : ListBuffer[Restaurant]): Any = {
+  def countByCuisine(restaurantList : List[Restaurant]): List[(String,Int)] = {
 
-      val res = restaurantList.map(_.typesOfCuisines.split(" "));
+    val cuisines = restaurantList.flatMap(_.typesOfCuisine).distinct
 
-      val cuisines = res.flatten.distinct
-
-      cuisines.map(x => (x,restaurantList.count(_.typesOfCuisines.contains(x))) )
+    cuisines.map({case x => x -> restaurantList.count(resturant => resturant.typesOfCuisine.contains(x))})
 
   }
+  val zomatoData = readData()
+
+  val res = countByCuisine(zomatoData)
+
+  println(res)
 
 }
--- src/test/scala/zomatoSuite.scala
+++ src/test/scala/zomatoSuite.scala
@@ -1,35 +1,32 @@
 import org.scalatest.FunSuite
 import zomato.Restaurant
 
-import scala.collection.mutable._
-
-
 class zomatoSuite extends FunSuite {
 
-  // Dummy data for testing
-  val dummyData: ListBuffer[Restaurant] = ListBuffer(
-    Restaurant("0", "A", "3.5/5.0", "300", "Mumbai", "Cafe", "Pastry Coffee", "Italian", "200"),
-    Restaurant("1", "B", "4.5/5.0", "100", "Delhi", "Cafe", "Pastry Ice-Cream Coffee", "Mixed", "300"),
-    Restaurant("2", "C", "4.2/5.0", "200", "Delhi", "Cafe", "Pastry Coffee", "North-Indian Mexican", "150"),
-    Restaurant("3", "D", "1.0/5.0", "350", "Delhi", "Pizzeria", "Pastry Coffee", "North-Indian Italian", "100"),
-    Restaurant("4", "E", "4.4/5.0", "230", "Mumbai", "Bar", "Pastry Coffee", "Chinese", "200"),
-    Restaurant("5", "F", "4.2/5.0", "120", "Delhi", "Pizzeria", "Pastry Coffee Biscuits", "Chinese Italian", "100")
+   //Dummy data for testing
+  val dummyData: List[Restaurant] = List(
+    Restaurant("0", "A", 3.5, 300, "Mumbai", "Cafe", List("Pastry","Coffee"), List("Italian"), "200"),
+    Restaurant("1", "B", 4.5, 100, "Delhi", "Cafe",  List("Pastry","Ice-Cream","Coffee"), List("Mixed"), "300"),
+    Restaurant("2", "C", 4.2, 200, "Delhi", "Cafe", List("Pastry","Coffee"), List("North-Indian","Mexican"), "150"),
+    Restaurant("3", "D", 1.0, 350, "Delhi", "Pizzeria", List("Pastry","Coffee"), List("North-Indian","Italian"), "100"),
+    Restaurant("4", "E", 4.4, 230, "Mumbai", "Bar", List("Pastry","Coffee"), List("Chinese"), "200"),
+    Restaurant("5", "F", 4.2, 120, "Delhi", "Pizzeria", List("Pastry","Coffee","Biscuits"), List("Chinese","Italian"), "100")
   )
 
   test("Test : Top N restaurants by rating") {
 
-    assert(zomato.sortByRating(2, dummyData) === ListBuffer(
-      Restaurant("1", "B", "4.5/5.0", "100", "Delhi", "Cafe", "Pastry Ice-Cream Coffee", "Mixed", "300"),
-      Restaurant("4", "E", "4.4/5.0", "230", "Mumbai", "Bar", "Pastry Coffee", "Chinese", "200"))
+    assert(zomato.sortByRating(2, dummyData) === List(
+      Restaurant("1", "B", 4.5, 100, "Delhi", "Cafe",  List("Pastry","Ice-Cream","Coffee"), List("Mixed"), "300"),
+      Restaurant("4", "E", 4.4, 230, "Mumbai", "Bar", List("Pastry","Coffee"), List("Chinese"), "200"))
     )
 
   }
 
   test("Test : Top N restaurants by rating in a given location and restaurant type") {
 
-    assert(zomato.locationAndTypeFilter(3, dummyData, "Cafe", "Delhi") === ListBuffer(
-      Restaurant("1", "B", "4.5/5.0", "100", "Delhi", "Cafe", "Pastry Ice-Cream Coffee", "Mixed", "300"),
-      Restaurant("2", "C", "4.2/5.0", "200", "Delhi", "Cafe", "Pastry Coffee", "North-Indian Mexican", "150")
+    assert(zomato.locationAndTypeFilter(3, dummyData, "Cafe", "Delhi") === List(
+      Restaurant("1", "B", 4.5, 100, "Delhi", "Cafe",  List("Pastry","Ice-Cream","Coffee"), List("Mixed"), "300"),
+      Restaurant("2", "C", 4.2, 200, "Delhi", "Cafe", List("Pastry","Coffee"), List("North-Indian","Mexican"), "150")
      )
 
     )
@@ -38,18 +35,18 @@ class zomatoSuite extends FunSuite {
 
   test("Test : Top N restaurants by rating and least number of votes in a given location") {
 
-    assert(zomato.filterByVote(4, dummyData, "Delhi") === ListBuffer(
-      Restaurant("1", "B", "4.5/5.0", "100", "Delhi", "Cafe", "Pastry Ice-Cream Coffee", "Mixed", "300"),
-      Restaurant("5", "F", "4.2/5.0", "120", "Delhi", "Pizzeria", "Pastry Coffee Biscuits", "Chinese Italian", "100"),
-      Restaurant("2", "C", "4.2/5.0", "200", "Delhi", "Cafe", "Pastry Coffee", "North-Indian Mexican", "150"),
-      Restaurant("3", "D", "1.0/5.0", "350", "Delhi", "Pizzeria", "Pastry Coffee", "North-Indian Italian", "100"),
+    assert(zomato.filterByVote(4, dummyData, "Delhi") === List(
+      Restaurant("1", "B", 4.5, 100, "Delhi", "Cafe",  List("Pastry","Ice-Cream","Coffee"), List("Mixed"), "300"),
+      Restaurant("5", "F", 4.2, 120, "Delhi", "Pizzeria", List("Pastry","Coffee","Biscuits"), List("Chinese","Italian"), "100"),
+      Restaurant("2", "C", 4.2, 200, "Delhi", "Cafe", List("Pastry","Coffee"), List("North-Indian","Mexican"), "150"),
+      Restaurant("3", "D", 1.0, 350, "Delhi", "Pizzeria", List("Pastry","Coffee"), List("North-Indian","Italian"), "100")
     ))
 
   }
 
   test("Test : No. of dishes liked in every restaurant") {
 
-    assert(zomato.likedDishCount(dummyData) === ListBuffer(2, 3, 2, 2, 2, 3))
+    assert(zomato.likedDishCount(dummyData) === List(2, 3, 2, 2, 2, 3))
 
   }
   test("Test : No. of distinct locations") {
@@ -66,14 +63,14 @@ class zomatoSuite extends FunSuite {
 
   test("Test : No. of distinct cuisines at each location") {
 
-    assert(zomato.allDistinctCuisine(dummyData) === List(4,2)
+    assert(zomato.allDistinctCuisine(dummyData) === Map("Delhi" -> 4, "Mumbai" -> 2)
     )
 
   }
 
   test("Test : Count of restaurants for each cuisine type") {
 
-    assert(zomato.countByCuisine(dummyData) === ListBuffer(("Italian",3), ("Mixed",1), ("North-Indian",2), ("Mexican",1), ("Chinese",2))
+    assert(zomato.countByCuisine(dummyData) === List(("Italian",3), ("Mixed",1), ("North-Indian",2), ("Mexican",1), ("Chinese",2))
     )
   }
 
